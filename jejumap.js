7ecb6fbdced099b99fecd8110b9bd4590c18f151
Biolbam <34931425+Biolbam@users.noreply.github.com>
2018-02-08 16:06:44 +0900

    Add files via upload

---
+++ jejumap.js
@@ -0,0 +1,22 @@
+eqfeed_callback({
+"type": "FeatureCollection",
+
+"features": [
+{ "type": "Feature", "properties": { "Name": "잃어버린 마을 곤을동", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ 126.558088888889003, 33.522155555555599, 0.0 ] } }
+,
+{ "type": "Feature", "properties": { "Name": "너븐숭이4.3기념관", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ 126.6887105, 33.5460108, 0.0 ] } }
+,
+{ "type": "Feature", "properties": { "Name": "제주4·3평화공원", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ 126.6189414, 33.4511596, 0.0 ] } }
+,
+{ "type": "Feature", "properties": { "Name": "다랑쉬굴", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ 126.830780555556004, 33.467433333333297, 0.0 ] } }
+,
+{ "type": "Feature", "properties": { "Name": "송령이골", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ 126.704886111110994, 33.314572222222203, 0.0 ] } }
+,
+{ "type": "Feature", "properties": { "Name": "진아영 할머니 삶터", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ 126.214683333332999, 33.375927777777797, 0.0 ] } }
+,
+{ "type": "Feature", "properties": { "Name": "큰넓궤", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ 126.323508333332995, 33.319888888888897, 0.0 ] } }
+,
+{ "type": "Feature", "properties": { "Name": "섯알오름", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ 126.280738888889005, 33.205672222222198, 0.0 ] } }
+
+]
+})
